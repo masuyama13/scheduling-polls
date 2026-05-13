d830283e12d7a67093f802abc56d15b74701b2fc
masuyama13 <masterxwebm13@gmail.com>
2026-05-12 18:33:00 -0700

    Add HomePage

---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+function HomePage() {
+  return (
+    <div className="home-page">
+      <main className="mx-auto flex min-h-screen max-w-3xl flex-col gap-10 px-6 py-12">
+      </main>
+    </div>
+  )
+}
+
+export default HomePage
